17f2645221830534e43e54837fa45eb1be002355
Alex Chernov <chernov.email@yandex.ru>
2020-07-30 20:21:27 +0300

    исправлен роут deleteCard

--- routes/cards.js
+++ routes/cards.js
@@ -18,7 +18,7 @@ cardsRouter.route('/cards')
       }),
     }),
   }), createCard);
-cardsRouter.delete('/cards/:_id', auth,
+cardsRouter.deleteCard('/cards/:_id', auth,
   celebrate({
     params: Joi.object().keys({
       cardId: Joi.string().length(24).hex(),
